d6b710fdd3b39c24577c8a32768bd6718c463b84
Nadia Pillard <ua9ond@yandex.ru>
2016-08-23 23:53:50 +0700

    let filteredPictures global done, setScrollEnabled function corrected

--- src/pictures/pictures.js
+++ src/pictures/pictures.js
@@ -24,7 +24,7 @@ let pageNumber = 0;
 
 /** @param {string} filter */
 let setFilterEnabled = (filter) => {
-  let filteredPictures = getFilteredPictures(pictures, filter);
+  filteredPictures = getFilteredPictures(pictures, filter);
   pageNumber = 0;
   renderPictures(filteredPictures, pageNumber, true);
   if (utils.isBottomReached() && utils.isNextPageAvailable(pictures, pageNumber, PAGE_SIZE)) {
@@ -32,6 +32,7 @@ let setFilterEnabled = (filter) => {
     renderPictures(filteredPictures, pageNumber, false);
   };
   gallery.getPhotos(filteredPictures);
+  
 }
 
 let setFiltrationEnabled = () => {
@@ -42,7 +43,7 @@ let setFiltrationEnabled = () => {
     let filterId = filtres[i].getAttribute('id');
 
     //создание надписи с кол-вом фотографий, подходящих под этот фильтр
-    let filteredPictures = getFilteredPictures(pictures, filterId);
+    filteredPictures = getFilteredPictures(pictures, filterId);
     let picturesQuantity = filteredPictures.length;
 
     //добавление disabled фильтрам, которым не соответствует ни одна фоо
@@ -65,7 +66,7 @@ let setFiltrationEnabled = () => {
   if (filterBlock.classList.contains('invisible')) {
     filterBlock.classList.remove('invisible');
   }
-
+  
 };
 
 let setScrollEnabled = () => {
@@ -73,23 +74,19 @@ let setScrollEnabled = () => {
   window.addEventListener('scroll', function () {
     if (Date.now() - lastCall >= THROTTLE_DELAY) {
       if (utils.isBottomReached() &&
-        utils.isNextPageAvailable(pictures, pageNumber, PAGE_SIZE)) {
+        utils.isNextPageAvailable(filteredPictures, pageNumber, PAGE_SIZE)) {
         pageNumber++;
-        renderPictures(pictures, pageNumber, false);
+        renderPictures(filteredPictures, pageNumber, false);
       }
       lastCall = Date.now();
     }
   });
 };
 
-
-
-
-
 getPictures(function (loadedPictures) {
   pictures = loadedPictures;
   setFiltrationEnabled();
-  setFilterEnabled('LIKES');
+  setFilterEnabled('filter-popular');
   setScrollEnabled();
   
 });
