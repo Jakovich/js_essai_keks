8529458fa049863d890d1c5cb6379c6bfd588700
Nadia Pillard <ua9ond@yandex.ru>
2016-11-16 02:45:17 +0700

    Methods in prototypes added

--- src/gallery.js
+++ src/gallery.js
@@ -2,7 +2,7 @@
 
 let Gallery = function() {
   let self = this;
-  const ESC_KEY = 27; //код клавиши ESC
+  
   this.photoGallery = document.querySelector('.gallery-overlay');
   this.galleryClose = this.photoGallery.querySelector('.gallery-overlay-close');
   this.galleryImg = this.photoGallery.querySelector('.gallery-overlay-image');
@@ -20,81 +20,96 @@ let Gallery = function() {
   @param {Array.<Object>} arr
   */
   this.getPhotos = (arr) => this.photosArray = arr.slice();
+
+  this._onPhotoClick = this._onPhotoClick.bind(this);
+  this._onDocumentKeyDown = this._onDocumentKeyDown.bind(this);
+  this.hideGallery = this.hideGallery.bind(this);
+  this.showGallery = this.showGallery.bind(this);
+  this.showPhoto = this.showPhoto.bind(this);
+
   
-  /**
+
+  
+  window.addEventListener('hashchange', this.hashVerify.bind(this));
+}
+
+Gallery.prototype._onPhotoClick = function() {
+  ++this.numberPhoto;
+  if (this.numberPhoto > this.photosArray.length - 1) {
+    this.numberPhoto = 0;
+  }
+  location.hash = 'photo/' + this.photosArray[this.numberPhoto].url;
+};
+
+Gallery.prototype.hashVerify = function() {
+  if (location.hash && location.hash.match(/#photo\/(\S+)/)) {
+    var hashLink = location.hash.match(/#photo\/(\S+)/);
+    this.showGallery(hashLink[1]);
+  }
+};
+
+/**
+  @param {Event} event
+  */
+
+Gallery.prototype._onDocumentKeyDown = function(event) {
+  const ESC_KEY = 27; //код клавиши ESC
+  if (event.keyCode === ESC_KEY && !this.photoGallery.classList.contains('invisible')) {
+    this.hideGallery();
+  }
+};
+
+/**
+  функция скрытия галереи
+*/
+Gallery.prototype.hideGallery = function() {
+  history.pushState('', document.title, window.location.pathname);
+  this.photoGallery.classList.add('invisible');
+  window.removeEventListener('keydown', this._onDocumentKeyDown);
+  this.galleryClose.removeEventListener('click', this.hideGallery);
+  this.galleryImg.removeEventListener('click', this._onPhotoClick);
+};
+
+/**
   функция показа галереи
   @param {number} indexPhoto
   */
 
-  this.showGallery = (indexPhoto) => {
-    if (self.photoGallery.classList.contains('invisible')) {
-      self.photoGallery.classList.remove('invisible');
-    }
-    self.showPhoto(indexPhoto);
-    window.addEventListener('keydown', self._onDocumentKeyDown);
-    self.galleryClose.addEventListener('click', self.hideGallery);
-    self.galleryImg.addEventListener('click', self._onPhotoClick);
+Gallery.prototype.showGallery = function(indexPhoto) {
+  if (this.photoGallery.classList.contains('invisible')) {
+    this.photoGallery.classList.remove('invisible');
   }
-  /**
+  this.showPhoto(indexPhoto);
+  window.addEventListener('keydown', this._onDocumentKeyDown);
+  this.galleryClose.addEventListener('click', this.hideGallery);
+  this.galleryImg.addEventListener('click', this._onPhotoClick);
+};
+
+/**
   функция показа изображения
   @param {number} num
   */
-  
-  this.showPhoto = (num) => {
-    if (typeof(num) === 'number') {
-      self.numberPhoto = num;
-      self.galleryImg.src = self.photosArray[num].url;
-      self.galleryLikes.innerHTML = self.photosArray[num].likes;
-      self.galleryComments.innerHTML = self.photosArray[num].comments;
-    } else if (typeof(num) === 'string') {
-      self.galleryImg.src = num;
-      self.photosArray.forEach(function(item, i){
-        if (item.url === num) {
-          self.numberPhoto = i;
-        }
-      })
-      self.galleryLikes.innerHTML = self.photosArray[self.numberPhoto].likes;
-      self.galleryComments.innerHTML = self.photosArray[self.numberPhoto].comments;
-    }
+
+Gallery.prototype.showPhoto = function(num) {
+  if (typeof(num) === 'number') {
+    this.numberPhoto = num;
+    this.galleryImg.src = this.photosArray[num].url;
+    this.galleryLikes.innerHTML = this.photosArray[num].likes;
+    this.galleryComments.innerHTML = this.photosArray[num].comments;
+  } else if (typeof(num) === 'string') {
+    this.galleryImg.src = num;
+    this.photosArray.forEach(function(item, i){
+      if (item.url === num) {
+        this.numberPhoto = i;
+      }
+    }.bind(this))
+    this.galleryLikes.innerHTML = this.photosArray[this.numberPhoto].likes;
+    this.galleryComments.innerHTML = this.photosArray[this.numberPhoto].comments;
   }
-  
-  /**
-  функция скрытия галереи
-  */
-
-  this.hideGallery = () => {
-    history.pushState('', document.title, window.location.pathname);
-    self.photoGallery.classList.add('invisible');
-    window.removeEventListener('keydown', self._onDocumentKeyDown);
-    self.galleryClose.removeEventListener('click', self.hideGallery);
-    self.galleryImg.removeEventListener('click', self._onPhotoClick);
-  }
-
-  this._onPhotoClick = () => {
-    ++self.numberPhoto;
-    if (self.numberPhoto > self.photosArray.length - 1) {
-      self.numberPhoto = 0;
-    }
-    location.hash = 'photo/' + self.photosArray[self.numberPhoto].url;
-  };
-
-  /**
-  @param {Event} event
-  */
-  this._onDocumentKeyDown = (event) => {
-    if (event.keyCode === ESC_KEY && !self.photoGallery.classList.contains('invisible')) {
-      self.hideGallery();
-    }
-  };
-  
-  this.hashVerify = function() {
-    if (location.hash && location.hash.match(/#photo\/(\S+)/)) {
-      var hashLink = location.hash.match(/#photo\/(\S+)/);
-      this.showGallery(hashLink[1]);
-    }
-  };
-  
-  window.addEventListener('hashchange', this.hashVerify.bind(this));
 }
+
+
+
+
 
 module.exports = new Gallery();
